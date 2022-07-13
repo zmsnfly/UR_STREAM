117e49401cbbfc3b602f52dcb34681b44f5e11fd
zmsnfly <zmsnfly@gmail.com>
2022-07-13 14:39:29 +0800

    fix: 修改界面，完善界面逻辑

--- UR_STREAM/ViewModels/MainWindowViewModel.cs
+++ UR_STREAM/ViewModels/MainWindowViewModel.cs
@@ -24,92 +24,15 @@ namespace UR_STREAM.ViewModels
             IP = "192.168.122.10";
             Port = "30003";
             IsRunning = false;
+            IsConnecting = false;
+            CanConnect = true;
         }
 
-        private UInt32 TotalMsgLength = 3288596480;
+        private readonly UInt32 TotalMsgLength = 3288596480;
 
-        private RelayCommand startCommand;
-
-        public ICommand StartCommand
-        {
-            get
-            {
-                if (startCommand == null)
-                {
-                    startCommand = new RelayCommand(Start);
-                }
-
-                return startCommand;
-            }
-        }
         private Socket socketClient = null;
         private Thread threadClient = null;
-
-        private void Start()
-        {
-            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            if (IP == null || Port == null)
-            {
-                return;
-            }
-            IPAddress ipAddress = IPAddress.Parse(IP);
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, int.Parse(Port));
-            try
-            {
-                socketClient.Connect(ipEndPoint);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("连接失败" + e.Message);
-                return;
-            }
-            IsRunning = true;
-            threadClient = new Thread(RecieveMsg)
-            {
-                IsBackground = true
-            };
-            threadClient.Start();
-        }
-
-        private void RecieveMsg()
-        {
-            while (IsRunning)
-            {
-                byte[] arrMsgRec = new byte[1220];
-                int length;
-                try
-                {
-                    length = socketClient.Receive(arrMsgRec);
-                }
-                catch (SocketException)
-                {
-                    break;
-                }
-                catch (Exception)
-                {
-                    break;
-                }
-
-                if (length == 0)
-                {
-                    break;
-                }
-                else
-                {
-                    var total_msg_length = BitConverter.ToUInt32(arrMsgRec, 0);
-                    if (total_msg_length == TotalMsgLength)
-                    {
-                        Array.Reverse(arrMsgRec);
-                        X = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (32 * 8))).ToString();
-                        Y = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (33 * 8))).ToString();
-                        Z = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (34 * 8))).ToString();
-                        RX = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (35 * 8))).ToString();
-                        RY = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (36 * 8))).ToString();
-                        RZ = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (37 * 8))).ToString();
-                    }
-                }
-            }
-        }
+        private delegate string ConnectSocketDelegate(IPEndPoint ipep, Socket sock);
 
         private string port;
 
@@ -162,6 +85,96 @@ namespace UR_STREAM.ViewModels
 
         public string Title { get => title; set => SetProperty(ref title, value); }
 
+        private string j1;
+        public string J1 { get => j1; set => SetProperty(ref j1, value); }
+
+        private string j2;
+        public string J2 { get => j2; set => SetProperty(ref j2, value); }
+
+        private string j3;
+        public string J3 { get => j3; set => SetProperty(ref j3, value); }
+
+        private string j4;
+        public string J4 { get => j4; set => SetProperty(ref j4, value); }
+
+        private string j5;
+        public string J5 { get => j5; set => SetProperty(ref j5, value); }
+
+        private string j6;
+        public string J6 { get => j6; set => SetProperty(ref j6, value); }
+
+        private RelayCommand startCommand;
+
+        public ICommand StartCommand
+        {
+            get
+            {
+                if (startCommand == null)
+                {
+                    startCommand = new RelayCommand(Start);
+                }
+
+                return startCommand;
+            }
+        }
+
+
+        private void Start()
+        {
+            IsConnecting = true;
+            Thread threadConnect = new Thread(TryConnect);
+            threadConnect.Start();
+        }
+
+        private string ConnectSocket(IPEndPoint ipep, Socket sock)
+        {
+            string exmessage = "";
+            try
+            {
+                sock.Connect(ipep);
+            }
+            catch (Exception ex)
+            {
+                exmessage = ex.Message;
+            }
+            finally
+            {
+            }
+
+            return exmessage;
+        }
+        private void TryConnect()
+        {
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            if (IP == null || Port == null)
+            {
+                IsConnecting = false;
+                return;
+            }
+            IPAddress ipAddress = IPAddress.Parse(IP);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, int.Parse(Port));
+            ConnectSocketDelegate connect = ConnectSocket;
+            IAsyncResult asyncResult = connect.BeginInvoke(ipEndPoint, socketClient, null, null);
+            bool connectSuccess = asyncResult.AsyncWaitHandle.WaitOne(2000, false);
+            if (!connectSuccess)
+            {
+                IsConnecting = false;
+                CanConnect = true;
+                MessageBox.Show(string.Format("连接失败！错误信息：{0}", "连接超时"));
+            }
+            else
+            {
+                IsRunning = true;
+                threadClient = new Thread(RecieveMsg)
+                {
+                    IsBackground = true
+                };
+                threadClient.Start();
+                IsConnecting = false;
+            }
+
+        }
+
         private RelayCommand saveCommand;
 
         public ICommand SaveCommand
@@ -182,24 +195,6 @@ namespace UR_STREAM.ViewModels
 
         }
 
-        private string x;
-        public string X { get => x; set => SetProperty(ref x, value); }
-
-        private string y;
-        public string Y { get => y; set => SetProperty(ref y, value); }
-
-        private string z;
-        public string Z { get => z; set => SetProperty(ref z, value); }
-
-        private string rX;
-        public string RX { get => rX; set => SetProperty(ref rX, value); }
-
-        private string rY;
-        public string RY { get => rY; set => SetProperty(ref rY, value); }
-
-        private string rZ;
-        public string RZ { get => rZ; set => SetProperty(ref rZ, value); }
-
         private RelayCommand stopCommand;
 
         public ICommand StopCommand
@@ -217,12 +212,61 @@ namespace UR_STREAM.ViewModels
 
         private void Stop()
         {
-            socketClient.Shutdown(SocketShutdown.Both);
-            socketClient.Disconnect(true);
-
-            IsRunning = false;
 
             threadClient?.Abort();
+            socketClient.Shutdown(SocketShutdown.Both);
+            socketClient.Close();
+            IsRunning = false;
+            CanConnect = true;
         }
+
+        private void RecieveMsg()
+        {
+            while (IsRunning)
+            {
+                byte[] arrMsgRec = new byte[1220];
+                int length;
+                try
+                {
+                    length = socketClient.Receive(arrMsgRec);
+                }
+                catch (SocketException)
+                {
+                    break;
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+
+                if (length == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    var total_msg_length = BitConverter.ToUInt32(arrMsgRec, 0);
+                    if (total_msg_length == TotalMsgLength)
+                    {
+                        Array.Reverse(arrMsgRec);
+                        J1 = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (32 * 8))).ToString();
+                        J2 = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (33 * 8))).ToString();
+                        J3 = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (34 * 8))).ToString();
+                        J4 = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (35 * 8))).ToString();
+                        J5 = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (36 * 8))).ToString();
+                        J6 = Function.Radian2Degree(BitConverter.ToDouble(arrMsgRec, arrMsgRec.Length - 4 - (37 * 8))).ToString();
+                    }
+                }
+            }
+        }
+
+        private bool? isConnecting;
+
+        public bool? IsConnecting { get => isConnecting; set => SetProperty(ref isConnecting, value); }
+
+        private bool? canConnect;
+
+        public bool? CanConnect { get => canConnect; set => SetProperty(ref canConnect, value); }
+
     }
 }
--- UR_STREAM/Views/MainWindow.xaml.cs
+++ UR_STREAM/Views/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -18,13 +19,23 @@ namespace UR_STREAM
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private readonly MainWindowViewModel MV = null;
+
         public MainWindow()
         {
             InitializeComponent();
-            MainWindowViewModel MV = new MainWindowViewModel();
+            MV = new MainWindowViewModel();
             this.DataContext = MV;
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton)
+            {
+                MV.CanConnect = false;
+            }
         }
     }
 }
